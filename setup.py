3c1e728da25e43bcfa655924519ee65968eb36fa
Jonas Schuepfer <jonasschuepfer@gmail.com>
2019-03-03 03:18:54 +0100

    Added first version of setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name="lava",
+    version="1.0.0",
+    description="Highlevel Wrapper for Vulkan's Compute API",
+    author="Jonas Schuepfer",
+    author_email="jonasschuepfer@gmail.com",
+    packages=["lava"],
+    include_package_data=True,
+    # install_requires=['cffi>=1.10'],
+    # setup_requires=["vulkan"],
+    # url="https://github.com/osanj/lava",
+    # keywords="Vulkan",
+    # classifiers=[
+    #     "Development Status :: 5 - Production/Stable",
+    #     "Intended Audience :: Developers",
+    #     "License :: OSI Approved :: Apache Software License",
+    #     "Operating System :: Android",
+    #     "Operating System :: Microsoft :: Windows",
+    #     "Operating System :: POSIX :: Linux",
+    #     "Natural Language :: English",
+    #     "Topic :: Multimedia :: Graphics",
+    #     "Topic :: Scientific/Engineering",
+    #     "Topic :: Software Development :: Libraries :: Python Modules",
+    # ]
+)
